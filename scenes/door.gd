aff0def77970487949f8dd67aec9b96da2669020
plackett <johnnicholasdimitri@gmail.com>
2025-04-05 19:39:44 -0400

    level 0 concept finished with level selection cave

--- scenes/door.gd
+++ scenes/door.gd
@@ -9,10 +9,14 @@ func _on_body_entered(body: Node2D) -> void:
 	$Sprite2D.texture = load("res://assets/door2.png")
 	accepting = true
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if not accepting: return
-	if Input.is_action_pressed("ui_up"):
-		get_tree().change_scene_to_file($newScene)
+	if not Input.is_action_pressed("ui_up"): return
+	if get_meta("levelOrPos") == true:
+		get_tree().change_scene_to_file(get_meta("newScene"))
+		return
+	get_tree().get_first_node_in_group("Player").global_position = get_meta("dest")
+	$AudioStreamPlayer.play(0)
 
 func _on_body_exited(body: Node2D) -> void:
 	if(not body.is_class("CharacterBody2D")):
