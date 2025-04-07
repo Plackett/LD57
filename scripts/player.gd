34a5156d8767c7f136ac7acd56481c371ebc9758
plackett <johnnicholasdimitri@gmail.com>
2025-04-07 16:06:55 -0400

    added pause menu icons

--- scripts/player.gd
+++ scripts/player.gd
@@ -83,4 +83,6 @@ func _physics_process(delta: float) -> void:
 		die()
 	
 func die():
+	up_direction = Vector2.UP
+	PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, Vector2.DOWN)
 	get_tree().reload_current_scene()
