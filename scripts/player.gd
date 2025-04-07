f5dd12e01e3299e4745bb068e3d6194b3658342d
plackett <johnnicholasdimitri@gmail.com>
2025-04-07 00:19:50 -0400

    fixed animations and gravity

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 250.0
 const JUMP_VELOCITY = -370.0
 
+@onready var _animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 # Reference to the timer
 @onready var jump_height_timer = $JumpHeightTimer
 
@@ -30,17 +32,36 @@ func _apply_impulses():
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	velocity += get_gravity() * delta 
+	# rotate due to gravity
+	$AnimatedSprite2D.rotation = get_gravity().angle() - PI/2
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		jump_height_timer.start()
-		velocity.y = JUMP_VELOCITY
+		if get_gravity().y > 0: velocity.y = JUMP_VELOCITY
+		else: velocity.y = JUMP_VELOCITY * -1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Get the input direction: -1, 0, 1
+	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	# Flip the sprite
+	if (direction > 0 and get_gravity().y > 0) or (direction < 0 and get_gravity().y < 0):
+		_animated_sprite.flip_h = false
+	elif (direction < 0 and get_gravity().y > 0) or (direction > 0 and get_gravity().y < 0):
+		_animated_sprite.flip_h = true
+		
+	# Play animations
+	if is_on_floor():
+		if abs(direction) <= 0.05:
+			_animated_sprite.play("Idle")
+		else:
+			_animated_sprite.play("Walk")
+	else:
+		_animated_sprite.play("Jump")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
